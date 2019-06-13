41a42fef3eb264b09d38f967eaa430ff57cdcd56
tt577 <yangtq2014@gmail.com>
2019-06-13 11:07:44 -0700

    create file

---
+++ fetchData.py
@@ -0,0 +1,13 @@
+#######################################################
+#CIS 41B      Lab3
+#File Name:   db.py
+#Author:      Tianqi Yang
+#Time:        6/13/2019
+#Description: 
+#######################################################
+import requests
+from  bs4  import BeautifulSoup
+import re
+import json
+import sqlite3
+
