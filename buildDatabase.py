c7de01db57486069b14d53e55de6644f6dc55789
tt577 <yangtq2014@gmail.com>
2019-06-14 11:11:22 -0700

    update fectch function

--- buildDatabase.py
+++ buildDatabase.py
@@ -10,6 +10,7 @@ import requests
 from  bs4  import BeautifulSoup
 import sqlite3
 import time
+
 REGULAR='Regular'
 MIDGRADE='Midgrade'
 PREMIUM='Premium'
@@ -29,38 +30,39 @@ cur = conn.cursor()
 page = requests.get(MAINLINK)
 soup = BeautifulSoup(page.content, "lxml")
 
+startTime = time.time()
 #table 1
-cur.execute("DROP TABLE IF EXISTS area")
-cur.execute('''CREATE TABLE area(
+cur.execute("DROP TABLE IF EXISTS Area")
+cur.execute('''CREATE TABLE Area(
             areaCode INTEGER PRIMARY KEY UNIQUE,
             areaName TEXT)''')
 #save data to table 1
 for index, elem in enumerate(soup.find_all('option'), 1):
     if elem.get_text().strip() not in timeTypes: 
         links[elem.get_text().strip()] = f"https://www.eia.gov/dnav/pet/{elem['value']}"
-        cur.execute('''INSERT INTO area VALUES (?, ?)''', ( index , elem.get_text().strip() ))
+        cur.execute('''INSERT INTO Area VALUES (?, ?)''', ( index , elem.get_text().strip() ))
+        
 #table 2
-cur.execute("DROP TABLE IF EXISTS gasTypeTable")
-cur.execute('''CREATE TABLE gasTypeTable(
+cur.execute("DROP TABLE IF EXISTS GasType")
+cur.execute('''CREATE TABLE GasType(
             gasCode INTEGER PRIMARY KEY UNIQUE,
             gasName TEXT)''')
 #save data to table 2
 for index, elem in enumerate(gasTypes, 1):
-    cur.execute('''INSERT INTO gasTypeTable VALUES (?, ?)''', ( index , elem ))
+    cur.execute('''INSERT INTO GasType VALUES (?, ?)''', ( index , elem ))
 
 #table 3
-cur.execute("DROP TABLE IF EXISTS timeTypeTable")
-cur.execute('''CREATE TABLE timeTypeTable(
+cur.execute("DROP TABLE IF EXISTS TimeType")
+cur.execute('''CREATE TABLE TimeType(
             timeCode INTEGER PRIMARY KEY UNIQUE,
             timeName TEXT)''')
 #save data to table 3
 for index, elem in enumerate(timeTypes, 1):
-    cur.execute('''INSERT INTO timeTypeTable VALUES (?, ?)''', ( index , elem )) 
+    cur.execute('''INSERT INTO TimeType VALUES (?, ?)''', ( index , elem )) 
     
 #table 4
-cur.execute("DROP TABLE IF EXISTS record")
-cur.execute('''CREATE TABLE record(
-           id INTEGER NOT NULL PRIMARY KEY UNIQUE,
+cur.execute("DROP TABLE IF EXISTS Records")
+cur.execute('''CREATE TABLE Records(
            dateYear INTEGER,
            dateMonth INTEGER,
            date INTEGER,
@@ -68,13 +70,11 @@ cur.execute('''CREATE TABLE record(
            areaId INTEGER,
            gasType INTEGER,
            timeType INTEGER,
-           FOREIGN KEY (areaId) REFERENCES area(areaCode),
-           FOREIGN KEY(gasType) REFERENCES gasTypeTable(gasCode),
-           FOREIGN KEY(timeType) REFERENCES timeTypeTable(timeCode)  
+           FOREIGN KEY (areaId) REFERENCES Area(areaCode),
+           FOREIGN KEY(gasType) REFERENCES GasType(gasCode),
+           FOREIGN KEY(timeType) REFERENCES TimeType(timeCode)  
            )''')
-startTime = time.time()
 #save data to table 4
-index = 1
 areaIndex = 1
 month, date, price = '', '', ''
 for area_links in links.values():
@@ -102,10 +102,9 @@ for area_links in links.values():
                             for m in range(1, len(j), 2):
                                 month, date, price= j[m].get_text()[:2], j[m].get_text()[3:], j[m+1].get_text()
                                 if month != '' and date != '' and price != '':
-                                    #index, year, month, date, price, areaId, gasType, timeType                          
-                                    print( index, year, int(month), int(date), float(price), areaIndex, gasIndex, timeType)
-                                    cur.execute('INSERT INTO record VALUES (?, ? ,?, ?, ?, ?, ?, ?)', ( index, year, int(month), int(date), float(price), areaIndex, gasIndex, timeType))
-                                    index += 1
+                                    print( year, int(month), int(date), float(price), areaIndex, gasIndex, timeType)
+                                    cur.execute('INSERT INTO Records VALUES ( ? ,?, ?, ?, ?, ?, ?)',
+                                                ( year, int(month), int(date), float(price), areaIndex, gasIndex, timeType))
             elif timeType == 2:
                 for i in differentTime_soup.body.find_all('tr'):
                     k = i.find_all('td', class_='B4')
@@ -114,15 +113,10 @@ for area_links in links.values():
                             j = i.find_all('td')
                             year = j[0].get_text().strip()
                             for m in range(1, len(j)):
-                                price = j[m].get_text()
-                                print( index, int(year), m, float(j[m].get_text()), areaIndex, gasIndex, timeType)
-                                cur.execute('''INSERT INTO record 
-                                                (id, dateYear, dateMonth, price, areaId, gasType, timeType)
-                                                VALUES
-                                                (?, ?, ?, ?, ?, ?, ?)''',
-                                                ( index, int(year), m, float(j[m].get_text()), areaIndex, gasIndex, timeType)
-                                            )
-                                index += 1          
+                                print( int(year), m, float(j[m].get_text()), areaIndex, gasIndex, timeType)
+                                cur.execute('''INSERT INTO Records 
+                                                (dateYear, dateMonth, price, areaId, gasType, timeType) VALUES ( ?, ?, ?, ?, ?, ?)''',
+                                                ( int(year), m, float(j[m].get_text()), areaIndex, gasIndex, timeType))
             else:
                 for i in differentTime_soup.body.find_all('tr'):
                     k = i.find_all('td', class_='B4')
@@ -132,17 +126,14 @@ for area_links in links.values():
                             j = i.find_all('td')
                             for m in range(1, len(j)):
                                 if j[m].get_text() != '':
-                                    print( index, target_year, float(j[m].get_text() ), areaIndex, gasIndex, timeType)
-                                    cur.execute('''INSERT INTO record 
-                                                    (id, dateYear, price, areaId, gasType, timeType)
-                                                    VALUES
-                                                    (?, ?, ?, ?, ?, ?)''',
-                                                    ( index, target_year, float(j[m].get_text() ), areaIndex, gasIndex, timeType)
-                                                )
+                                    print( target_year, float(j[m].get_text() ), areaIndex, gasIndex, timeType)
+                                    cur.execute('''INSERT INTO Records 
+                                                    ( dateYear, price, areaId, gasType, timeType) VALUES (?, ?, ?, ?, ?)''',
+                                                    ( target_year, float(j[m].get_text() ), areaIndex, gasIndex, timeType))
                                     target_year+=1                                
-                                    index += 1                                
             timeType += 1
         gasIndex += 1
     areaIndex += 1
 print(f'Time:{round(time.time()-startTime,2)}s')
 conn.commit()
+
--- fetchData.py
+++ fetchData.py
@@ -5,3 +5,42 @@
 #Time:        6/13/2019
 #Description: fetch data from database
 #######################################################
+import sqlite3
+REGULAR='Regular'
+MIDGRADE='Midgrade'
+PREMIUM='Premium'
+WEEKLY='Weekly'
+MONTHLY='Monthly'
+ANNUAL='Annual'
+gasTypes=( REGULAR, MIDGRADE, PREMIUM )
+timeTypes=( WEEKLY, MONTHLY, ANNUAL)
+class fetcher:
+    def __init__(self):
+        self._conn = sqlite3.connect('data.db')
+        self._cur = self._conn.cursor()
+        self._cur.execute("PRAGMA table_info(Records)")
+        self._areaList = self.getArea()
+
+    def close(self):
+        self._conn.commit()
+        self._conn.close()
+        
+    def getArea(self):
+        self._cur.execute('SELECT areaName FROM Area')
+        return [i[0] for i in self._cur.fetchall()]
+    
+    def getRecordsByAreaGasTime(self, areaIndex, gasIndex, timeIndex):
+        '''
+        fetch data by area, gas, and time and return a list of data
+        '''
+        self._cur.execute('''SELECT Records.dateYear, Records.dateMonth, Records.date, Records.price
+                            FROM Records JOIN Area JOIN GasType JOIN TimeType 
+                            ON Records.areaId = Area.areaCode AND Area.areaName = "{}"
+                            AND Records.gasType = GasType.gasCode AND GasType.gasName = "{}"
+                            AND Records.timeType = TimeType.timeCode AND TimeType.timeName = "{}" 
+                          '''.format(self._areaList[areaIndex], gasTypes[gasIndex], timeTypes[timeIndex] ))
+        return self._cur.fetchall()
+    
+f = fetcher()
+print(f.getRecordsByAreaGasTime(1,1,1))#Monthly, East Coast (PADD 1)
+f.close()
