f8ad5ee6b5882193bedbb404f8d9264ba6a8a529
bubblemans <f89647779@gmail.com>
2019-06-19 17:28:03 -0700

    add a window for prediction and a local server calcate

--- predict.py
+++ predict.py
@@ -1,14 +1,11 @@
-from pandas import DataFrame
-from pandas import concat
-import csv
-import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers.recurrent import LSTM
-from keras.layers.core import Dense, Dropout
-from math import sqrt
-from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt 
+import tkinter as tk
+import fetchData
+import socket
+import pickle
+
+HOST = "localhost"      # on the same machine   
+PORT = 5551             # as long as this is unassigned
 
 # parse a csv file
 # param: filename
@@ -23,118 +20,72 @@ def parse(filename):
 		records.reverse()
 		return records[:-5]
 
-class Predict():
+class PredictWin(tk.Tk):
 	def __init__(self):
-		pass
- 
-	def series_to_supervised(self, data, n_in=1, n_out=1, dropnan=True):
-		"""
-		Frame a time series as a supervised learning dataset.
-		Arguments:
-			data: Sequence of observations as a list or NumPy array.
-			n_in: Number of lag observations as input (X).
-			n_out: Number of observations as output (y).
-			dropnan: Boolean whether or not to drop rows with NaN values.
-		Returns:
-			Pandas DataFrame of series framed for supervised learning.
-		author: Jason Brownlee
-		"""
-		n_vars = 1 if type(data) is list else data.shape[1]
-		df = DataFrame(data)
-		cols, names = list(), list()
-		# input sequence (t-n, ... t-1)
-		for i in range(n_in, 0, -1):
-			cols.append(df.shift(i))
-			names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-		# forecast sequence (t, t+1, ... t+n)
-		for i in range(0, n_out):
-			cols.append(df.shift(-i))
-			if i == 0:
-				names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-			else:
-				names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-		# put it all together
-		agg = concat(cols, axis=1)
-		agg.columns = names
-		# drop rows with NaN values
-		if dropnan:
-			agg.dropna(inplace=True)
-		return agg
+		'''show the main window'''
+		super().__init__()
+		self._f = fetchData.fetcher()
+		self._areas = dict( (y,x) for x,y in self._f.getAreaWithNum())
+		self._gas = dict( (y,x) for x,y in self._f.getGasWithNum())
 
-	# use machine learning to predict the next period's gas price
-	# param: a list of gas prices in float
-	# return: the next period's gas price and root mean square error
-	def predictError(self, values):
-		# convert time-seried to supervised
-		reframed = self.series_to_supervised(values)
-		# print(values)
-		# print(reframed)
+        # set window
+		self.geometry("500x250+600+300")
+		self.title("Predict")
+		self.grab_set()
+		self.focus_set()
 
-		scaler = MinMaxScaler(feature_range=(0, 1))
-		scaled = scaler.fit_transform(reframed)
+		# period type
+		pframe = tk.Frame(self)
+		tk.Label(pframe, text='Time period').grid(row=0, column=0, sticky="w")
+		period = ["Weekly", "Monthly", "Yearly"]
+		self._time = tk.StringVar()
+		self._time.set(period[0])
+		tk.OptionMenu(pframe, self._time, *period ).grid(row=0, column=1, padx=10)
+		pframe.pack(pady=10)
 
-		# split into train and test sets
-		values = scaled
-		# n_train_days = 2 * 365
-		n_train_days = 900
-		train = values[:n_train_days, :]
-		test = values[n_train_days:, :]
-		# split into input and outputs
-		train_X, train_y = train[:, :-1], train[:, -1]
-		test_X, test_y = test[:, :-1], test[:, -1]
-		# reshape input to be 3D [samples, timesteps, features]
-		train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-		test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-		# print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+		# area
+		frame = tk.Frame(self)
+		tk.Label(frame, text='You live in :').grid(row=0, column=0, sticky="w")
+		area = [ i for i in self._areas.keys() ]
+		self._live = tk.StringVar()
+		self._live.set(area[0])
+		tk.OptionMenu(frame, self._live, *area ).grid(row=0, column=1, padx=10)
+		frame.pack(pady=10)
 
-		# design network
-		model = Sequential()
-		model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
-		model.add(Dense(1))
-		model.compile(loss='mae', optimizer='adam')
-		# fit network
-		history = model.fit(train_X, train_y, epochs=50, batch_size=91, validation_data=(test_X, test_y), verbose=2,
-		                    shuffle=False)
+		# gas type
+		frame2 = tk.Frame(self)
+		tk.Label(frame2, text='gas type is:').grid(row=0, column=0, sticky="w")
+		gas = [ i for i in self._gas.keys() ]
+		self._gasType = tk.StringVar()
+		self._gasType.set(gas[0])
+		tk.OptionMenu(frame2, self._gasType, *gas).grid(row=0, column=1, padx=10)
+		frame2.pack(pady=10)
 
-		# make a prediction
-		yhat = model.predict(test_X)
-		# print(yhat)
-		test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
-		# invert scaling for forecast
-		inv_yhat = np.concatenate((test_X[:, 0:], yhat), axis=1)
-		inv_yhat = scaler.inverse_transform(inv_yhat)
-		inv_yhat = inv_yhat[:, -1]
-		# print(inv_yhat)
-		# invert scaling for actual
-		test_y = test_y.reshape((len(test_y), 1))
-		inv_y = np.concatenate((test_X[:, 0:], test_y), axis=1)
-		inv_y = scaler.inverse_transform(inv_y)
-		inv_y = inv_y[:, -1]
-		# calculate RMSE
-		rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+		# Predict button
+		button = tk.Button(self, text="Predict", command=self.handlePredict)
+		button.pack(pady=10)
 
-		# for i in range(len(inv_yhat)):
-		# 	print(inv_y[i], inv_yhat[i])
-		# print(inv_yhat[-1])
-		return inv_yhat[-1], rmse
+		# result label
+		self.result = tk.Label(self)
+		self.result.pack(pady=10)
+
+	def handlePredict(self):
+		with socket.socket() as s:
+			s.connect((HOST, PORT))
+			mesg = str(self._areas[self._live.get()]-1) + "," + str(self._gas[self._gasType.get()]-1) + "," + self._time.get()
+			s.send(mesg.encode('utf-8'))
+			fromServer = pickle.loads(s.recv(1024))
+			self.updateLabel("The next "+self._time.get()[0:-2]+" gas price "+"is "+str(fromServer))
+			s.close()
+
+	def updateLabel(self, fromServer):
+		self.result.config(text=fromServer)
+		self.update()
+
 
 if __name__ == '__main__':
-	predict_obj = Predict()
-	# get data
-	testList = parse("Regular.csv")
-	values = [float(row[1]) for row in testList]
-	reg_predcit, reg_error = predict_obj.predictError(values)
-
-	testList = parse("Midgrade.csv")
-	values = [float(row[1]) for row in testList]
-	mid_predict, mid_error = predict_obj.predictError(values)
-
-	testList = parse("Premium.csv")
-	values = [float(row[1]) for row in testList]
-	pre_predict, pre_error = predict_obj.predictError(values)
-
-	print(reg_error, mid_error, pre_error, (reg_error+mid_error+pre_error)/3)
-	print(reg_predcit, mid_predict, pre_predict)
+	win = PredictWin()
+	win.mainloop()
 
 	
 
---
+++ server.py
@@ -0,0 +1,167 @@
+from pandas import DataFrame
+from pandas import concat
+import csv
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers.recurrent import LSTM
+from keras.layers.core import Dense, Dropout
+from math import sqrt
+from sklearn.metrics import mean_squared_error
+import socket
+import fetchData
+import pickle
+
+HOST = "localhost"      # on the same machine   
+PORT = 5551             # as long as this is unassigned
+
+class Predict():
+	def __init__(self):
+		self._f = fetchData.fetcher()
+		pass
+ 
+	def series_to_supervised(self, data, n_in=1, n_out=1, dropnan=True):
+		"""
+		Frame a time series as a supervised learning dataset.
+		Arguments:
+			data: Sequence of observations as a list or NumPy array.
+			n_in: Number of lag observations as input (X).
+			n_out: Number of observations as output (y).
+			dropnan: Boolean whether or not to drop rows with NaN values.
+		Returns:
+			Pandas DataFrame of series framed for supervised learning.
+		author: Jason Brownlee
+		"""
+		n_vars = 1 if type(data) is list else data.shape[1]
+		df = DataFrame(data)
+		cols, names = list(), list()
+		# input sequence (t-n, ... t-1)
+		for i in range(n_in, 0, -1):
+			cols.append(df.shift(i))
+			names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+		# forecast sequence (t, t+1, ... t+n)
+		for i in range(0, n_out):
+			cols.append(df.shift(-i))
+			if i == 0:
+				names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+			else:
+				names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+		# put it all together
+		agg = concat(cols, axis=1)
+		agg.columns = names
+		# drop rows with NaN values
+		if dropnan:
+			agg.dropna(inplace=True)
+		return agg
+
+	# use machine learning to predict the next period's gas price
+	# param: a list of gas prices in float
+	# return: the next period's gas price and root mean square error
+	def predictError(self, values):
+		# convert time-seried to supervised
+		reframed = self.series_to_supervised(values)
+		# print(values)
+		# print(reframed)
+
+		scaler = MinMaxScaler(feature_range=(0, 1))
+		scaled = scaler.fit_transform(reframed)
+
+		# split into train and test sets
+		values = scaled
+		# n_train_days = 2 * 365
+		n_train_days = int(len(values) * 0.9)
+		train = values[:n_train_days, :]
+		test = values[n_train_days:, :]
+		# split into input and outputs
+		train_X, train_y = train[:, :-1], train[:, -1]
+		test_X, test_y = test[:, :-1], test[:, -1]
+		# reshape input to be 3D [samples, timesteps, features]
+		train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+		test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+		# print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+		# design network
+		model = Sequential()
+		model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+		model.add(Dense(1))
+		model.compile(loss='mae', optimizer='adam')
+		# fit network
+		history = model.fit(train_X, train_y, epochs=50, batch_size=91, validation_data=(test_X, test_y), verbose=2,
+		                    shuffle=False)
+
+		# make a prediction
+		yhat = model.predict(test_X)
+		# print(yhat)
+		test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+		# invert scaling for forecast
+		inv_yhat = np.concatenate((test_X[:, 0:], yhat), axis=1)
+		inv_yhat = scaler.inverse_transform(inv_yhat)
+		inv_yhat = inv_yhat[:, -1]
+		# print(inv_yhat)
+		# invert scaling for actual
+		test_y = test_y.reshape((len(test_y), 1))
+		inv_y = np.concatenate((test_X[:, 0:], test_y), axis=1)
+		inv_y = scaler.inverse_transform(inv_y)
+		inv_y = inv_y[:, -1]
+		# calculate RMSE
+		rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+
+		# for i in range(len(inv_yhat)):
+		# 	print(inv_y[i], inv_yhat[i])
+		# print(inv_yhat[-1])
+		return inv_yhat[-1], rmse
+
+	def fetchData(self, fromClient):
+		params = fromClient.split(",")
+		period = 0
+		if params[2] == "Weekly":
+			period = 0
+		elif params[2] == "Monthly":
+			period = 1
+		elif params[2] == "Yearly":
+			period = 2
+
+		data = self._f.getRecordsByAreaGasTime(int(params[0]), int(params[1]), int(period))
+		values = [float(row[3]) for row in data]
+		result, error = self.predictError(values)
+
+		return result
+
+
+
+if __name__ == '__main__':
+	predict_obj = Predict()
+	# # get data
+	# testList = parse("Regular.csv")
+	# values = [float(row[1]) for row in testList]
+	# reg_predcit, reg_error = predict_obj.predictError(values)
+
+	# testList = parse("Midgrade.csv")
+	# values = [float(row[1]) for row in testList]
+	# mid_predict, mid_error = predict_obj.predictError(values)
+
+	# testList = parse("Premium.csv")
+	# values = [float(row[1]) for row in testList]
+	# pre_predict, pre_error = predict_obj.predictError(values)
+
+	# print(reg_error, mid_error, pre_error, (reg_error+mid_error+pre_error)/3)
+	# print(reg_predcit, mid_predict, pre_predict)
+
+	with socket.socket() as s:
+		s.bind((HOST, PORT))
+		while True:
+			s.listen()
+			print("server ready")
+			(conn, addr) = s.accept()
+			fromClient = conn.recv(1024).decode('utf-8')
+			mesg = predict_obj.fetchData(fromClient)
+			conn.send(pickle.dumps(mesg))
+
+
+
+
+
+
+
+	
+
